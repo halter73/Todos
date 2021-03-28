ab0b70f1cd73681d80cd2f3ecef47d9a2dfc9338
Stephen Halter <halter73@gmail.com>
2021-03-28 13:49:48 -0700

    Remove extra projects

--- TodoBasicWithAuth/TodoUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Todos
-{
-    public class TodoUser : IdentityUser
-    {
-        public bool IsAdmin { get; set; }
-    }
-}
--- TodoWithControllersAuthJWT/TodoUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace TodoWithControllersAuthJWT
-{
-    public class TodoUser : IdentityUser
-    {
-        public bool IsAdmin { get; set; }
-    }
-}
